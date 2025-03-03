ef22b0d06246e353df9fc99a79752e2fb18cb6f2
Alolote <alemfort12@gmail.com>
2025-03-03 21:51:37 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "📦 Installing Travis Package..."
+
+# Install dependencies
+pkg install openssl curl tar -y
+
+# Create a bin folder
+mkdir -p /data/data/com.termux/files/usr/bin/
+
+# Move scripts to bin folder
+cp travis /data/data/com.termux/files/usr/bin/
+cp travis_start /data/data/com.termux/files/usr/bin/
+cp travis_join /data/data/com.termux/files/usr/bin/
+
+# Make scripts executable
+chmod +x /data/data/com.termux/files/usr/bin/travis*
+echo "✅ Installation Complete! Run 'travis' to start."
